ad1c80198955b11475dd7a9fdb2c9854b639cef1
William Mooney Sloneker <wmsloneker@gmail.com>
2019-10-20 23:01:11 -0400

    Built initial server to listen to and respond to client requests

---
+++ server.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+var express = require("express");
+var path = require("path");
+
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+require("/api/friends")(app);
+
+// LISTENER
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
