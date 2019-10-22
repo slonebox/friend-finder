ac0337ce6e2b171ca3f574e007667cf0b14fd3b4
William Mooney Sloneker <wmsloneker@gmail.com>
2019-10-22 13:48:28 -0400

    Established data structure for friends array

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friendsArray = [
+    {
+    name:"Fiona",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,5,5,5,5,5,5,5,5,5
+      ]
+  }
+];
+
+module.exports = friendsArray;
