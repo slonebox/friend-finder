73d2786efec41d4316e689050fce0616cd25c853
William Mooney Sloneker <wmsloneker@gmail.com>
2019-10-25 23:36:44 -0400

    Implemented function that identifies a best match friend from the existing friendsArray and logs that object to the console

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,30 +19,45 @@ module.exports = function (app) {
       scores: req.body.scores.map(x => parseFloat(x))
     };
 
-    //Function to find a best match
-    function bestMatch(newObj, array) {
-      var bestMatch;
-      var bestScore;
-      var totalDifference = 0;
+    //Function defined to find a best match
+    function bestMatch(obj, array) {
+      //Empty variables to store best match and best score
+      var allDifferences = [];
 
-      //Logic that calculates total difference between two friends scores
-      for (i = 0; i < 10; i++) {
-        
-        var questionDifference = Math.abs(newObj.scores[i] - array[0].scores[i]);
-        totalDifference += questionDifference;
+      //For loop that passes through each object in array of existing friends
+      for (i = 0; i < array.length; i++) {
+
+        var userScores = obj.scores;
+        var existingScores = array[i].scores;
+        var totalDifference =
+          Math.abs(userScores[0] - existingScores[0]) +
+          Math.abs(userScores[1] - existingScores[1]) +
+          Math.abs(userScores[2] - existingScores[2]) +
+          Math.abs(userScores[3] - existingScores[3]) +
+          Math.abs(userScores[4] - existingScores[4]) +
+          Math.abs(userScores[5] - existingScores[5]) +
+          Math.abs(userScores[6] - existingScores[6]) +
+          Math.abs(userScores[7] - existingScores[7]) +
+          Math.abs(userScores[8] - existingScores[8]) +
+          Math.abs(userScores[9] - existingScores[9]);
+
+          allDifferences.push(totalDifference);
       };
 
+      var bestScore = Math.min.apply(Math, allDifferences);
+      var bestMatchIndex = allDifferences.indexOf(bestScore);
+
+      console.log("BEST MATCH: " + JSON.stringify(array[bestMatchIndex]));
     };
 
+    //Function called to determine best match
     bestMatch(newFriend, friendsArray);
 
-
+    //New user inputs pushed to array
     friendsArray.push(newFriend);
     res.json(true);
     console.log(friendsArray);
   });
-
-
 
 };
 
